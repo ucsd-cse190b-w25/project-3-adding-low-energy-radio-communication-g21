7169d73f6fb2535a78e8fc23e42cbcd17d2fe30b
beliangg <b6liang@ucsd.edu>
2025-03-14 02:35:32 -0700

    Changed to 1 Mhz clock and changed timer.c to reflect

--- youlostit-ble/Core/Src/main.c
+++ youlostit-ble/Core/Src/main.c
@@ -246,7 +246,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
   // This lines changes system clock frequency
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_7;
+  RCC_OscInitStruct.MSIClockRange = RCC_CR_MSIRANGE_4; // changes to 1 MHz
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
